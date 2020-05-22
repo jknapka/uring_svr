2d0545616308a867dbb848fcf8d8b2161935b114
jo <jknapka@kneuro.net>
2020-05-22 11:02:01 -0400

    Added some larger test cases.

--- test/test_suite.sh
+++ test/test_suite.sh
@@ -48,21 +48,56 @@ function verify_files() {
 	done
 }
 
-# Test case 1.
+echo Test case 1.
 start_uring_svr 16K 1
 fetch_files 1 data/nums.txt
 wait $URING_SVR_PID
 verify_files 1 data/nums.txt
+rm -f bucket/*
+echo
 
-# Test case 2,
+echo Test case 2,
 start_uring_svr 1024K 20
 fetch_files 20 data/nums.txt
 wait $URING_SVR_PID
 verify_files 20 data/nums.txt
+rm -f bucket/*
+echo
 
-# Test case 3.
+echo Test case 3.
 start_uring_svr 1024K 1
 fetch_files 1 data/r3.dat
 wait $URING_SVR_PID
 verify_files 1 data/r3.dat
+rm -f bucket/*
+echo
+
+echo Test case 4: transfer 120 copies of tiny.dat.
+start_uring_svr 1024K 120
+fetch_files 120 data/tiny.dat
+wait $URING_SVR_PID
+verify_files 120 data/tiny.dat
+rm bucket/*
+echo
+
+echo Big test case - generate a 1GB file and transfer it.
+dd if=/dev/urandom of=data/1G.dat bs=1024 count=$(expr 1024 \* 1024)
+start_uring_svr 1024K 2
+fetch_files 2 data/1G.dat
+wait $URING_SVR_PID
+verify_files 2 data/1G.dat
+rm -f bucket/*
+echo
+
+echo Append a bit onto 1G.dat and fetch again.
+echo 0123456789 >> data/1G.dat
+start_uring_svr 16384B 1
+fetch_files 1 data/1G.dat
+wait $URING_SVR_PID
+verify_files 1 data/1G.dat
+rm -f bucket/*
+
+echo Deleting 1G.dat...
+rm -f data/1G.dat
+echo Done.
 
